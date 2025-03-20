fa628ec4d31a2f6bdb39e6a5787c11be2ba6e330
AlexTurboG <alexander.geo.georgiev@gmail.com>
2025-03-20 16:57:18 +0200

    Added footer and navbar

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -7,9 +7,29 @@ function Home() {
     const [title, setTitle] = useState("");
 
     return (
-        <div>
-            <h1>Welcome to the Home Page</h1>
-        </div>
+        <body>
+            {/*navbar*/}
+            <div class="navbar">
+                <a class="name" href="#">Logo</a>
+                <ul class="ul_navbar">
+                    <li><div class="item item-1"><a href="#">Home</a></div></li>
+                    <li><div class="item item-2"><a href="#">Transatctions</a></div></li>
+                    <li><div class="item item-3"><a href="#">Info</a></div></li>
+                </ul>
+                <div class="something_container">
+                <a class="something Login" href="Login.jsx">Log in</a>
+                <a class="something Signup" href="Register.jsx">Sign Up</a>
+                </div>
+            </div>
+            {/*End of navbar*/}
+            {/*footer*/}
+            <div class="footer">
+                <p class="text text1">&#169;2025 Let me code.<br></br>All rigths reserved </p>
+
+                <p class="something something2">Something</p>
+            </div> 
+        
+        </body>
     );
 }
 
