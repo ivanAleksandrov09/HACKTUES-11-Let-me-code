74caca7df6b5b6f5c96fc723a78439034fb3d023
IvanDjumbishliev <ivan.n.dzhumbishliev.2023@elsys-bg.org>
2025-03-20 18:29:11 +0200

    Added PDF inputter

---
+++ frontend/src/pages/PDF_enter.jsx
@@ -0,0 +1,10 @@
+export default function PDFenter() {
+    return (
+        <div className="input_box">
+            <h1 className="heading">PDF Enter</h1>
+            <p className="input_label">Upload your PDF file here.</p>
+            <input type="file" accept=".pdf" onChange={(event) => this.setState({ selectedFile: event.target.files[0] })} />
+            <button className="submit_btn">Submit</button>
+        </div>
+    )
+}
