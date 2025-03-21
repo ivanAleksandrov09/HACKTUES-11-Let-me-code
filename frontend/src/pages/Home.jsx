8347ca11ceca711de41330133a44b306b2ef7856
Martin-R-D <159622261+Martin-R-D@users.noreply.github.com>
2025-03-21 09:37:51 +0200

    Upadated chart in main branch

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -9,11 +9,11 @@ import "../styles/Home.css"
 ChartJS.register(ArcElement, Tooltip, Legend);
 
 function Home() {
-    const [transactions, setTransactions] = useState([{'category': 'Food', 'cost': 100, 'date': '2024-01-01'}, 
-        {'category': 'Transport', 'cost': 200, 'date': '2024-01-02'}, 
-        {'category': 'Entertainment', 'cost': 300, 'date': '2024-01-03'}, 
-        {'category': 'Bills', 'cost': 400, 'date': '2024-01-04'}, 
-        {'category': 'Bills', 'cost': 500, 'date': '2024-01-05'}]);
+    const [transactions, setTransactions] = useState([{ 'category': 'Food', 'cost': 100, 'date': '2024-01-01' },
+    { 'category': 'Transport', 'cost': 200, 'date': '2024-01-02' },
+    { 'category': 'Entertainment', 'cost': 300, 'date': '2024-01-03' },
+    { 'category': 'Bills', 'cost': 400, 'date': '2024-01-04' },
+    { 'category': 'Bills', 'cost': 500, 'date': '2024-01-05' }]);
 
     // useEffect(() => {
     //     const fetchTransactionsdata = async () => {
@@ -29,37 +29,47 @@ function Home() {
 
 
     // Sample data for the pie chart
+    const possibleColors = [
+        "rgba(28, 28, 72, 1)",   // Muted Midnight Blue
+        "rgba(41, 51, 61, 1)",   // Deep Slate Gray
+        "rgba(66, 99, 146, 1)",  // Soft Steel Blue
+        "rgba(87, 135, 125, 1)", // Gentle Pine Green
+        "rgba(124, 165, 115, 1)",// Mossy Green
+        "rgba(189, 204, 99, 1)", // Muted Lemon Yellow
+        "rgba(224, 206, 149, 1)",// Soft Sand Beige
+        "rgba(197, 145, 91, 1)", // Matte Copper
+        "rgba(199, 111, 107, 1)",// Earthy Coral
+        "rgba(180, 142, 157, 1)",// Dusty Rose
+        "rgba(160, 112, 186, 1)",// Subtle Mauve
+        "rgba(117, 76, 148, 1)", // Matte Amethyst
+        "rgba(77, 62, 102, 1)",  // Smoky Violet
+        "rgba(64, 61, 85, 1)",   // Charcoal Purple
+        "rgba(41, 41, 56, 1)"    // Ashy Deep Gray
+    ];
+
+    function makeBackgroundColors(transactionsSet) {
+        const transactionCategories = Array.from(transactionsSet);
+        let dictionary = {};
+
+        // Assign a color to each unique category
+        transactionCategories.forEach((category, index) => {
+            dictionary[category] = possibleColors[index];
+        });
+
+        return dictionary;
+    }
+    // Sample data for the pie chart
+    const transactionsSet = new Set(transactions.map(transaction => transaction.category));
+    const backgroundColors = makeBackgroundColors(transactionsSet);
+    const x = transactions.map(transaction => backgroundColors[transaction.category]);
+    console.log(x);
     const chartData = {
         labels: [...transactions.map(transaction => transaction.category)],
         datasets: [
             {
                 data: [...transactions.map(transaction => transaction.cost)],
-                backgroundColor: [
-                    "rgba(28, 28, 72, 0.8)",   // Muted Midnight Blue
-                    "rgba(41, 51, 61, 0.8)",   // Deep Slate Gray
-                    "rgba(66, 99, 146, 0.8)",  // Soft Steel Blue
-                    "rgba(87, 135, 125, 0.8)", // Gentle Pine Green
-                    "rgba(124, 165, 115, 0.8)",// Mossy Green
-                    "rgba(189, 204, 99, 0.8)", // Muted Lemon Yellow
-                    "rgba(224, 206, 149, 0.8)",// Soft Sand Beige
-                    "rgba(197, 145, 91, 0.8)", // Matte Copper
-                    "rgba(199, 111, 107, 0.8)",// Earthy Coral
-                    "rgba(180, 142, 157, 0.8)",// Dusty Rose
-                    "rgba(160, 112, 186, 0.8)",// Subtle Mauve
-                ],
-                borderColor: [
-                    "rgba(28, 28, 72, 1)",   // Muted Midnight Blue
-                    "rgba(41, 51, 61, 1)",   // Deep Slate Gray
-                    "rgba(66, 99, 146, 1)",  // Soft Steel Blue
-                    "rgba(87, 135, 125, 1)", // Gentle Pine Green
-                    "rgba(124, 165, 115, 1)",// Mossy Green
-                    "rgba(189, 204, 99, 1)", // Muted Lemon Yellow
-                    "rgba(224, 206, 149, 1)",// Soft Sand Beige
-                    "rgba(197, 145, 91, 1)", // Matte Copper
-                    "rgba(199, 111, 107, 1)",// Earthy Coral
-                    "rgba(180, 142, 157, 1)",// Dusty Rose
-                    "rgba(160, 112, 186, 1)",// Subtle Mauve
-                ],
+                backgroundColor: x,
+                borderColor: 'rgb(255, 255, 255)',
                 borderWidth: 1,
             },
         ],
@@ -70,6 +80,43 @@ function Home() {
         plugins: {
             legend: {
                 position: 'right',
+                labels: {
+                    font: {
+                        size: 14,
+                        family: 'Arial'
+                    },
+                    color: '#333',
+                    padding: 20,
+                    usePointStyle: true,
+                    generateLabels: function (chart) {
+                        const data = chart.data;
+                        let uniqueLabels = new Set();
+                        let result = [];
+
+                        if (data.labels.length && data.datasets.length) {
+                            data.labels.forEach((label, i) => {
+                                if (!uniqueLabels.has(label)) {
+                                    uniqueLabels.add(label);
+                                    result.push({
+                                        text: `${label}: $${data.datasets[0].data[i]}`,
+                                        fillStyle: data.datasets[0].backgroundColor[i],
+                                        hidden: false,
+                                        lineCap: 'round',
+                                        lineDash: [],
+                                        lineDashOffset: 0,
+                                        lineJoin: 'round',
+                                        lineWidth: 3,
+                                        strokeStyle: data.datasets[0].backgroundColor[i],
+                                        pointStyle: 'circle',
+                                        index: i
+                                    });
+                                }
+                            });
+                            return result;
+                        }
+                        return [];
+                    }
+                }
             },
             title: {
                 display: true,
@@ -82,9 +129,13 @@ function Home() {
     };
 
     const printRecentTransactions = () => {
-        return transactions.map((transaction, index) => (
-            <div key={index}>{index + 1}. Category: {transaction.category}, Cost: {transaction.cost}, Date:  {transaction.date}</div>
-        ));
+        return [...transactions]
+            .sort((a, b) => new Date(b.date) - new Date(a.date))
+            .map((transaction, index) => (
+                <div key={index}>{index + 1}. Category: {transaction.category},
+                    Cost: {transaction.cost},
+                    Date:  {transaction.date}</div>
+            ));
     };
 
 
@@ -99,8 +150,8 @@ function Home() {
                     <li><div className="item item-3"><Link to="/">Info</Link></div></li>
                 </ul>
                 <div className="something_container">
-                <Link className="something Login" to="/Login">Log in</Link>
-                <Link className="something Signup" to="/Register">Sign Up</Link>
+                    <Link className="something Login" to="/Login">Log in</Link>
+                    <Link className="something Signup" to="/Register">Sign Up</Link>
                 </div>
             </div>
             {/*End of navbar*/}
@@ -109,29 +160,29 @@ function Home() {
                 <p classNameName="text text1">&#169;2025 Let me code.<br></br>All rigths reserved </p>
 
                 <p className="something something2">Something</p>
-            </div> 
+            </div>
 
 
             <div className="dashboard-container">
-            <div className="dashboard-content">
-                <div className="dashboard-card">
-                    <h1 className="dashboard-title">Welcome to Your Dashboard</h1>
-                    <div className="dashboard-grid">
-                        <div className="chart-container">
-                            <h2 className="chart-title">Expense Overview</h2>
-                            <div className="chart-wrapper">
-                                <Pie data={chartData} options={options} />
+                <div className="dashboard-content">
+                    <div className="dashboard-card">
+                        <h1 className="dashboard-title">Welcome to Your Dashboard</h1>
+                        <div className="dashboard-grid">
+                            <div className="chart-container">
+                                <h2 className="chart-title">Expense Overview</h2>
+                                <div className="chart-wrapper">
+                                    <Pie data={chartData} options={options} />
+                                </div>
                             </div>
-                        </div>
-                        <div className="transactions-container">
-                            <h2 className="transactions-title">Recent Transactions</h2>
-                            {printRecentTransactions()}
+                            <div className="transactions-container">
+                                <h2 className="transactions-title">Recent Transactions</h2>
+                                {printRecentTransactions()}
 
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
         </>
     );
 }
