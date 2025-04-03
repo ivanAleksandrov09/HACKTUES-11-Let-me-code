cc5c0b4cf2368cf63a4cec90989cb395e9b4a410
Ivan Alexandrov <ivan.d.aleksandrov.2023@elsys-bg.org>
2025-04-03 14:11:50 +0300

    refactor: correct class name, improve code structure, and change caching to apply only to leaflet files to allow for custom prompts

--- backend/api/views/leaflet.py
+++ backend/api/views/leaflet.py
@@ -1,14 +1,15 @@
-import requests
-from bs4 import BeautifulSoup
-from .client import client
 import io
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework.permissions import AllowAny
-from django.core.cache import cache
-import requests
 import urllib.request
 from datetime import datetime, timedelta
+
+import requests
+from bs4 import BeautifulSoup
+from django.core.cache import cache
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from .client import client
 
 
 # Fetch the leaflets from supported websites
@@ -16,6 +17,7 @@ class LeafletView(APIView):
     permission_classes = [AllowAny]
 
     def get(self, request, *args, **kwargs):
+        cache_used = False
         request_date = datetime.today()
 
         cache_key = f"{request_date}"
@@ -24,53 +26,53 @@ class LeafletView(APIView):
         last_fetch = cache.get(last_fetch_key)
 
         if last_fetch and (request_date - last_fetch) < timedelta(days=7):
+            cache_used = True
             cache_key = last_fetch
-            return Response(
-                cache.get(cache_key), status=200, headers={"X-Cache": "HIT"}
+            files = cache.get(cache_key)
+        else:
+            url = "https://www.kaufland.bg/broshuri.html"
+            try:
+                page = requests.get(url)
+            except:
+                raise Exception("Couldn't access kaufland download page.")
+
+            soup = str(BeautifulSoup(page.text, "html.parser"))
+
+            returned_urls = client.models.generate_content(
+                model="gemini-2.0-flash",
+                contents=[
+                    soup,
+                    "In the attached html script you can access the leaflets webpage of a supermarket. I need you to extract the download urls for each pdf leaflet from it. Please don't explain your thought process and provide only the urls separated by commas.",
+                ],
             )
 
-        url = "https://www.kaufland.bg/broshuri.html"
-        try:
-            page = requests.get(url)
-        except:
-            raise Exception("Couldn't access kaufland download page.")
+            urls = returned_urls.text.split(",")
 
-        soup = str(BeautifulSoup(page.text, "html.parser"))
+            # remove newlines added from split method
+            urls = [url.strip() for url in urls]
 
-        returned_urls = client.models.generate_content(
-            model="gemini-2.0-flash",
-            contents=[
-                soup,
-                "In the attached html script you can access the leaflets webpage of a supermarket. I need you to extract the download urls for each pdf leaflet from it. Please don't explain your thought process and provide only the urls separated by commas.",
-            ],
-        )
+            files = []
+            for current_url in urls:
+                # download the pdf leaflet
+                file_bytes = urllib.request.urlopen(current_url).read()
 
-        urls = returned_urls.text.split(",")
+                ai_uploaded_file = client.files.upload(
+                    file=(io.BytesIO(file_bytes)),
+                    config={"mime_type": "application/pdf"},
+                )
 
-        # remove newlines added from split method
-        urls = [url.strip() for url in urls]
-
-        files = []
-        for current_url in urls:
-            # download the pdf leaflet
-            file_bytes = urllib.request.urlopen(current_url).read()
-
-            ai_uploaded_file = client.files.upload(
-                file=(io.BytesIO(file_bytes)),
-                config={"mime_type": "application/pdf"},
-            )
-
-            files.append(ai_uploaded_file)
+                files.append(ai_uploaded_file)
 
         response = client.models.generate_content(
             model="gemini-2.0-flash",
             contents=[
                 files,
-                "I want you to list the 5 biggest discounts you find from the given leaflets. Don't explain your thought process.",
+                "I want you to list the 5 best deals you find from the given leaflets. Don't explain your thought process.",
             ],
         )
 
-        cache.set(last_fetch_key, request_date, timeout=14 * 24 * 60 * 60)
-        cache.set(cache_key, response.text, timeout=14 * 24 * 60 * 60)
+        if not cache_used:
+            cache.set(last_fetch_key, request_date, timeout=14 * 24 * 60 * 60)
+            cache.set(cache_key, files, timeout=14 * 24 * 60 * 60)
 
         return Response({"response": response.text}, status=201)
