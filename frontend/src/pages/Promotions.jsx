64859057e71c753e5cfa1f86836cced43d16d5f7
Martin-R-D <159622261+Martin-R-D@users.noreply.github.com>
2025-04-22 00:18:14 +0300

    Added suspence for the best deals

--- frontend/src/pages/Promotions.jsx
+++ frontend/src/pages/Promotions.jsx
@@ -2,13 +2,16 @@ import Deals from "../components/Deals";
 import DealSearcher from "../components/DealSearcher";
 import "../styles/pages/Promotion.css";
 import { useState } from "react";
+import { Suspense } from "react";
 
 function Promotions() {
   const [dealsFetched, setDealsFetched] = useState(false);
   return (
     <div className="promotions-container fade-in">
       <div style={{ minHeight: "90vh" }}>
-        <Deals isFetched={() => setDealsFetched(true)}/>
+        <Suspense fallback={<div id='loadingDeals'>Loading...</div>}>
+          <Deals isFetched={() => setDealsFetched(true)}/>
+        </Suspense>
       </div>
       <div style={{ minHeight: "90vh" }}>
         {dealsFetched && <DealSearcher />}
